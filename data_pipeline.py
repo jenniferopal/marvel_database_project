b473e4be890d7cdff91c31266eda61941c668212
Jennifer Opal <contact@jenniferopal.co>
2024-08-26 19:47:07 +0100

    data transformation script draft

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,43 +1,86 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
 from apache_beam.io import ReadFromPubSub
-from apache_beam.io.gcp.bigquery import WriteToBigQuery
+from collections import Counter
 import json
-
-# Define constants
 
 project_id = "marveldatabaseproject"
 topic_name = "marveldatabasetopic"
-subscription = "projects/marveldatabaseproject/subscriptions/marveldatabasetopic-sub ".format(project_id,
-                                                                                              'marveldatabasetopic-sub')
+csv_files = [
+    "../marvel_database_project/datasets/edges.csv",
+    "../marvel_database_project/datasets/hero-network.csv",
+    "../marvel_database_project/datasets/nodes.csv"
+]
 
-# Schema for your BigQuery table
+# parse JSON data from Pub/Sub messages
 
-schema = {
-    'fields': [
-        {'name': 'hero', 'type': 'STRING'},
-        {'name': 'comic', 'type': 'STRING'},
-    ]
-}
-
-
-# Pipeline steps
-class ParsePubSubMessage(beam.DoFn):
+class ParseJsonFn(beam.DoFn):
     def process(self, element):
-        message = json.loads(element.decode('utf-8'))
-        yield {
-            'hero': message['hero'],
-            'comic': message['comic'],
+        return [json.loads(element.decode('utf-8'))]
+
+# An example of transformation
+
+class FilterCharactersFn(beam.DoFn):
+    def process(self, element):
+        # Example: Filter characters with more than 5 connections
+        if len(element.get('connections', [])) > 5:
+            yield element
+
+
+class MarvelRelationshipTransformFn(beam.DoFn):
+    def process(self, element):
+        name = element.get('name', 'Unknown')
+        connections = element.get('connections', [])
+        groups = element.get('groups', [])
+
+        # Count the number of connections
+        connection_count = len(connections)
+
+        # Get the top 5 most frequent groups
+        top_groups = Counter(groups).most_common(5)
+
+        # Calculate a simple 'relationship strength' metric
+        # This could be based on number of shared comics, for example
+        relationship_strength = {conn: element.get('shared_comics', {}).get(conn, 0) for conn in connections}
+
+        # Sort connections by relationship strength
+        sorted_connections = sorted(relationship_strength.items(), key=lambda x: x[1], reverse=True)
+        top_connections = sorted_connections[:5]  # Get top 5 strongest connections
+
+        # Create the transformed element
+        transformed_element = {
+            'name': name,
+            'connection_count': connection_count,
+            'top_groups': top_groups,
+            'top_connections': top_connections,
+            'total_strength': sum(relationship_strength.values()),
+            'average_strength': sum(relationship_strength.values()) / len(
+                relationship_strength) if relationship_strength else 0
         }
 
+        yield transformed_element
+
+        # Emit relationship pairs for network analysis
+        for connected_char, strength in relationship_strength.items():
+            yield {
+                'character1': name,
+                'character2': connected_char,
+                'strength': strength
+            }
 
 def run():
-    # Set up the pipeline options
-    options = PipelineOptions(
-        runner = 'DataflowRunner',
-        project = project_id,
-        job_name = 'marvel-universe-pipeline',
-        temp_location = 'gs://temp_marveldatabase',
-        region = 'us-east-1'
-    )
+    # Set up pipeline options
+    options = PipelineOptions()
     options.view_as(StandardOptions).streaming = True
+
+    # Create the pipeline
+    with beam.Pipeline(options=options) as p:
+        # Read from Pub/Sub
+        messages = (p | "Read from Pub/Sub" >> ReadFromPubSub(topic="projects/marveldatabaseproject/topics/marveldatabasetopic")
+                    | "Parse JSON" >> beam.ParDo(ParseJsonFn())
+                    | "Filter Characters" >> beam.ParDo(FilterCharactersFn())
+                    | "Print Results" >> beam.Map(print))
+
+
+if __name__ == '__main__':
+    run()
